4dabef53664a194a04c39b4aa95f9cd877f4e5a6
Sarah Wright <sew104312@gmail.com>
2022-09-29 21:02:59 -0500

    Set up the framework for text input in an alert dialog

--- komekt_4/lib/home_screen.dart
+++ komekt_4/lib/home_screen.dart
@@ -1,7 +1,49 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+class HomeScreen extends StatefulWidget {
+  HomeScreen({super.key});
+  
+
+    @override
+  State<StatefulWidget> createState() =>_HomeScreenState();   
+}
+class _HomeScreenState extends State<HomeScreen>{
+  final TextEditingController _inputController = TextEditingController();
+  final ButtonStyle yesStyle = ElevatedButton.styleFrom(
+    textStyle: const TextStyle(fontSize:20),
+    primary: Colors.green
+  );
+  final ButtonStyle noStyle = ElevatedButton.styleFrom(
+    textStyle: const TextStyle(fontSize: 20),
+    primary:  Colors.red
+  );
+  Future<void> _displayTextInputDialog(BuildContext context){
+    return showDialog(context: context,
+     builder: (context){
+        return AlertDialog(
+          title: const Text ("Set the Game Name"),
+          content: TextField(
+            onChanged: (value){
+              setState(() {
+                game_name = value;
+              });
+            },
+            controller: _inputController,
+            decoration: 
+              const InputDecoration(hintText: "Type the game name"),
+          ),
+        );
+     })
+  }
+  
+String game_name = "";
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+}
 
   // This widget is the root of your application.
   @override
@@ -18,4 +60,4 @@ class HomeScreen extends StatelessWidget {
       ),
     );
   }
-}
+  
