d6895462ebbf4ad377fd05eb8e18df5130ef957c
Ted Bjurlin <teddybjurlin@gmail.com>
2022-10-05 14:33:52 -0500

    finished alert dialog ui

---
+++ komekt_4/lib/alert_dialogs.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/material.dart';
+
+class CustomAlert extends StatefulWidget {
+  const CustomAlert({super.key, required this.addFriend, this.friends});
+
+  final bool addFriend;
+
+  final Map<String, String>? friends; // type should be changed based on Friends implementation
+
+  @override
+  State<CustomAlert> createState() => _CustomAlertState();
+}
+
+class _CustomAlertState extends State<CustomAlert> {
+
+  final _formKey = GlobalKey<FormState>();
+  String _name = '';
+  String _ip = '';
+  
+  late String _dropdownvalue;
+
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: const Text('Item To Add'),
+      content:
+        Form(
+          autovalidateMode: AutovalidateMode.onUserInteraction,
+          key: _formKey,
+          child:
+            (widget.addFriend)
+            ?
+            Column( 
+              mainAxisSize: MainAxisSize.min,
+              children: [
+              TextFormField(
+                
+                decoration: const InputDecoration(
+                  labelText: 'Enter name...',
+                ),
+                onChanged: (text) => setState(() => _name = text),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter a name';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                  labelText: 'Enter IP address...',
+                ),
+                onChanged: (text) => setState(() => _ip = text),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter an IP address';
+                  }
+                  return null;
+                },
+              ),
+            ])
+            :
+            Column( children: [
+              DropdownButtonFormField(
+                hint: const Text('Please select a friend...'),
+                items: widget.friends!.values.map((String friend) { // needs to be replaced with corresponding map for Friends inplementation.
+                  return DropdownMenuItem(
+                    value: friend,
+                    child: Text(friend),
+                  );
+                }).toList(),
+                onChanged: (value) => setState(() => _dropdownvalue = value!),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please select a friend';
+                  }
+                  return null;
+                },
+                
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                  labelText: 'Enter game name...',
+                ),
+                onChanged: (text) => setState(() => _name = text),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter a game name';
+                  }
+                  return null;
+                },
+              ),
+            ])
+          ),
+      actions: <Widget>[
+        ElevatedButton(
+          onPressed: () {
+            if (_formKey.currentState!.validate()) {
+              if (widget.addFriend) {
+                // add friend to list
+              } else {
+                // create new game
+                // navigate to game screen
+              }
+            }
+          },
+          child: const Icon(Icons.check)
+        ),
+        ElevatedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: const Icon(Icons.cancel),
+        ),
+      ],
+    );
+  }
+}
--- komekt_4/lib/home_screen.dart
+++ komekt_4/lib/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:komekt_4/alert_dialogs.dart';
 
 class HomeScreen extends StatefulWidget {
   HomeScreen({super.key});
@@ -8,86 +9,21 @@ class HomeScreen extends StatefulWidget {
   State<StatefulWidget> createState() =>_HomeScreenState();   
 }
 class _HomeScreenState extends State<HomeScreen>{
-  final TextEditingController _inputController = TextEditingController();
-  var items = ['Testing','Friend 2','Friend 1'];
-  late String dropdownvalue;
 
   @override
   void initState(){
     super.initState();
-    dropdownvalue = 'Testing';
   }
-  final ButtonStyle yesStyle = ElevatedButton.styleFrom(
-    textStyle: const TextStyle(fontSize:20),
-    primary: Colors.green
-  );
-  final ButtonStyle noStyle = ElevatedButton.styleFrom(
-    textStyle: const TextStyle(fontSize: 20),
-    primary:  Colors.red
-  );
   
-    Future<void> _displayTextInputDialog(BuildContext context) async {
+  Future<void> _displayAlertDialog(BuildContext context, bool addFriend) async {
     return showDialog(
         context: context,
         builder: (context) {
-          return AlertDialog(
-            title: const Text("Put in the game name and choose which friend"),
-            content: TextField(
-              onChanged: (value) {
-                setState(() {
-                  game_name = value;
-                });
-              },
-              controller: _inputController,
-            ),
-            actions: <Widget>[
-            DropdownButton(
-             value: dropdownvalue,
-             icon:const Icon(Icons.keyboard_arrow_down),
-             items: items.map((String item){
-              return DropdownMenuItem(value: item,
-              child: Text(item),
-              );
-             }).toList(),
-              onChanged: (String? value) { 
-              setState(() {
-                dropdownvalue = value!;
-              });
-             },
-             ),
-            ValueListenableBuilder<TextEditingValue>(
-              valueListenable: _inputController,
-              builder: (context, value, child) {
-              return ElevatedButton(
-                key: const Key("OkButton"),
-                style: yesStyle,
-                onPressed: value.text.isNotEmpty
-                        ? () {
-                  setState(() {
-                    //_handleNewItem(valueText);
-                    Navigator.pop(context);
-                  });
-                } : null,
-                child: const Text('Accept'),
-              );
-              }
-              ),
-              ElevatedButton(
-                    key: const Key("CancelButton"),
-                    style: noStyle,
-                    child: const Text("Cancel"),
-                    onPressed: () {
-                            setState(() {
-                              Navigator.pop(context);
-                            });
-                          }                  
-                  )
-                  ]
-          );
+          return CustomAlert(addFriend: addFriend);
         });
   }
   
-String game_name = "Testing";
+  String game_name = "Testing";
 
   // This widget is the root of your application.
   @override
@@ -95,7 +31,7 @@ String game_name = "Testing";
     return Scaffold(
       appBar: AppBar(
         title: const Text('Komekt 4'),
-        actions: [IconButton(onPressed: (() {}), icon: const Icon(Icons.person_add))],
+        actions: [IconButton(onPressed: (() {_displayAlertDialog(context, true);}), icon: const Icon(Icons.person_add))],
         ),
       body: ListView(
         padding: const EdgeInsets.symmetric(vertical: 8.0),
@@ -108,7 +44,7 @@ String game_name = "Testing";
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: (() {
-          _displayTextInputDialog(context);
+          _displayAlertDialog(context, false); // needs to be passed friends object
         }),
         child: const Icon(Icons.add), 
       ),
