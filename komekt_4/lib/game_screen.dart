43c7931d19a493e2e89dd46bc97f1ce9dc90b445
Ted Bjurlin <teddybjurlin@gmail.com>
2022-10-07 12:12:33 -0500

    created the layout for the game board, as well as adjusted the ui of the game screen

--- komekt_4/lib/game_screen.dart
+++ komekt_4/lib/game_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:komekt_4/komekt_board.dart';
 
 class GameScreen extends StatelessWidget {
   GameScreen({super.key});
@@ -13,14 +14,44 @@ class GameScreen extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    const int _rows = 6;
+    const int _columns = 7;
+    double _buttonSize = MediaQuery.of(context).size.width * 0.1;
+
     return Scaffold(
-      appBar: AppBar(title: Text('Game Name Placeholder'),),
+      appBar: AppBar(title: const Text('Game Name Placeholder'),),
       body: Column(children: [
-        const Text('placeholder'),
-        const Text('Friend turn'),
+        Expanded(flex: 2, child: Container(color: Theme.of(context).backgroundColor)),
+        Center(
+          child: DecoratedBox(
+            decoration: BoxDecoration(
+              border: Border.all(width: 1.0, color: Colors.black38),
+            ),
+            child: const AspectRatio(
+              aspectRatio: 7/6,
+              child: Komekt4Board(
+                rows: _rows,
+                columns: _columns,
+              ),
+            ),
+          ),
+        ),
+        Expanded(flex: 5, child:
+          Container(
+            width: MediaQuery.of(context).size.width,
+            color: Theme.of(context).backgroundColor,
+            child: Column(children: const [
+              Spacer(flex: 2),
+              Text('Friend turn', textScaleFactor: 4,),
+              Spacer(flex: 3),
+            ],),
+        )),
+
+        
         Row(children: [
-          IconButton(onPressed: (() {}), icon: const Icon(Icons.done)),
-          IconButton(onPressed: (() {}), icon: const Icon(Icons.close)),
+          IconButton(iconSize: _buttonSize, onPressed: (() {}), icon: const Icon(Icons.close)),
+          const Spacer(),
+          IconButton(iconSize: _buttonSize, onPressed: (() {}), icon: const Icon(Icons.done)),
           ]),
       ]),
     );
